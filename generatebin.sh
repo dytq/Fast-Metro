7afbd9b3c53cfa746377939cd0157c7b20c342f9
dytq <taariq.deda@gmail.com>
2020-03-20 17:41:16 +0100

    Automatisation de livraison

---
+++ generatebin.sh
@@ -0,0 +1,7 @@
+mkdir releases;
+mkdir releases/app;
+cp -r data releases/app;
+cp -r image releases/app;
+cp target/fast-metro-0.0.1-SNAPSHOT.jar releases/app;
+cp README.pdf releases;
+cp -r Documentation releases/app; 
